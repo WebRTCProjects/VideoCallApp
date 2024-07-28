b9dbd79ecf5eb137010227c81d2fed4416f2abaf
Your Name <you@example.com>
2024-07-28 16:56:24 +0300

    Adding new EventListener to check user join to caht

--- backend/src/main/java/com/example/chatApp/config/WebSocketConfig.java
+++ backend/src/main/java/com/example/chatApp/config/WebSocketConfig.java
@@ -2,8 +2,6 @@ package com.example.chatApp.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
--- backend/src/main/java/com/example/chatApp/config/WebSocketEventListener.java
+++ backend/src/main/java/com/example/chatApp/config/WebSocketEventListener.java
@@ -9,6 +9,7 @@ import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 @Component
@@ -42,6 +43,23 @@ public class WebSocketEventListener {
             telegramService.sendMessage(chatMessage.getSender()+" left the chat");
             redisService.updateUsers(username);
 
+        }
+    }
+
+    @EventListener
+    public void handleNewUserJoined(SessionConnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+
+        if (username != null) {
+            log.info("User connected {}", username);
+
+            var chatMessage = ChatMessage.builder()
+                    .type(MessageType.JOIN)
+                    .sender(username)
+                    .build();
+            messageTemplate.convertAndSend("/topic/public",chatMessage );
+            telegramService.sendMessage(chatMessage.getSender()+" joined the chat");
         }
     }
 }
